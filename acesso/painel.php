bdbf70977af01ff0503c7f1aa817e5d22aef5d67
hermogenes-ciandt <hermogenes@ciandt.com>
2017-06-29 15:25:29 -0300

    Mascara CEP

--- acesso/painel.php
+++ acesso/painel.php
@@ -156,7 +156,7 @@ endif;
             $('.mask-date').mask('00/00/0000', {selectOnFocus: true});
             $('.mask-time').mask('00:00:00', {selectOnFocus: true});
             $('.mask-date_time').mask('00/00/0000 00:00:00', {selectOnFocus: true});
-            $('.mask-cep').mask('00000-000', {selectOnFocus: true});
+            $('.mask-cep').mask('00.000-000', {selectOnFocus: true});
             $('.mask-phone').mask('(00) 0000-0000', {selectOnFocus: true});
             $('.mask-cpf').mask('000.000.000-00', {reverse: true,selectOnFocus: true});
             $('.mask-cnpj').mask('00.000.000/0000-00', {reverse: true,selectOnFocus: true});
