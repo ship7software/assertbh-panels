15da9c5e1035d08eeab700966a836979442f0364
hermogenes-ciandt <hermogenes@ciandt.com>
2017-06-29 12:26:14 -0300

    Correções Imagem

--- acesso/__jsc/datatables-tabelas.js
+++ acesso/__jsc/datatables-tabelas.js
@@ -145,20 +145,4 @@ $(document).ready(function() {
         }
 
     });
-
-    $.mask.definitions['~']='[+-]';
-    //Inicio Mascara Telefone
-    $('input[type=tel]').focusout(function(){
-        var phone, element;
-        element = $(this);
-        element.unmask();
-        phone = element.val().replace(/\D/g, '');
-        if(phone.length > 10) {
-            element.mask("(99) 99999-999?9");
-        } else {
-            element.mask("(99) 9999-9999?9");
-        }
-    }).trigger('focusout');
-
-    $(".cep").mask("99.999-999");
 });
